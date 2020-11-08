73f91d8186f66bd73d380f1656ef5f44f9f06223
Rodrigo Hernández <rodrigo19-1_1998@hotmail.com>
2020-11-08 13:38:47 -0300

    k

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static(path.join(__dirname, '/web/')));
 
 const swaggerUi = require('swagger-ui-express'),
 swaggerDocument = require('./swagger.json');
